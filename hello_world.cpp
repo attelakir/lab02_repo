7218ad442aef34884230dc95386c21e6fef0de99
attelakir <71684236+attelakir@users.noreply.github.com>
2021-03-17 09:21:34 +0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+std::string name; //it's user's name
+std::cout << "Enter your name please !" << std::endl;
+std::cin >> name;
+std::cout << "Hello world " << name << " !"  << std::endl;
+}
