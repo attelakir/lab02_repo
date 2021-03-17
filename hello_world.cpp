5e646926191a5bd5fc40e5f2446e6190d381f685
attelakir <tretyak.polina10@mail.ru>
2021-03-17 10:39:05 +0300

    fix

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,7 @@
 #include <string>
 
 int main(){
-<<<<<<< HEAD
-std::string name; //its user's name
-=======
-std::string name; //its username
+std::string name;
 >>>>>>> edit
 std::cin >> name;
 std::cout << "Hello world from " << name << " !" << std::endl;
