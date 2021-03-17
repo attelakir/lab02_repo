014d98d41fed047fec04b53471773fbe52fd64fe
attelakir <71684236+attelakir@users.noreply.github.com>
2021-03-17 09:22:55 +0300

    Delete hello_world.cpp

--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main(){
-std::string name; //it's user's name
-std::cout << "Enter your name please !" << std::endl;
-std::cin >> name;
-std::cout << "Hello world " << name << " !"  << std::endl;
-}
