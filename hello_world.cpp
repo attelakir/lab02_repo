606d7afcdf4c0888d2f682a551bad1f62a0d0575
attelakir <tretyak.polina10@mail.ru>
2021-03-17 06:08:24 +0300

    added username to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main(){
-cout << "Hello world!" << endl;
+string name;
+cout << "Enter your name please !" << endl;
+cin >> name;
+cout << "Hello world " << name << " !"  << endl;
 }
