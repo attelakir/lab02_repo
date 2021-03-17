d194db4419cda6870b5bfcde8571284280ffd2bb
attelakir <71684236+attelakir@users.noreply.github.com>
2021-03-17 09:39:24 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-std::string name; //its username
+std::string name; //its user's name
 std::cin >> name;
 std::cout << "Hello world from " << name << " !" << std::endl;
 }
