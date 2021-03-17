87cc88e6246b47bf8bb96998799a2f26c74a9d1e
attelakir <tretyak.polina10@mail.ru>
2021-03-17 06:15:01 +0300

    added replay

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-std::string name;
+std::string name; //it's username
 std::cout << "Enter your name please !" << std::endl;
 std::cin >> name;
 std::cout << "Hello world " << name << " !"  << std::endl;
